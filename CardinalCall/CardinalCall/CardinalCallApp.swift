91010555154498a4b0331c05f8848b18c823ac65
daironghan <rongrong910@gmail.com>
2025-05-23 11:08:55 -0700

    Project init

---
+++ CardinalCall/CardinalCall/CardinalCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardinalCallApp.swift
+//  CardinalCall
+//
+//  Created by 韓岱融 on 2025/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CardinalCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
